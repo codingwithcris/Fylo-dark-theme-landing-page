eac05b8d808e02ba944de2e6d860d3c03381b14b
codingwithcris <codingwithcris@gmail.com>
2024-08-25 10:51:30 -0700

    Initial commit

---
+++ dist/index.4187be47.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".cta-form__button"),t=document.getElementById("email"),n=document.querySelector(".cta-form__error");e.addEventListener("click",function(){t.value.includes("@")||(n.style.display="block",n.innerText="Please enter a valid email address")});
+//# sourceMappingURL=index.4187be47.js.map
---
+++ dist/index.44983732.js
@@ -0,0 +1,15 @@
+// VARIABLES
+const submitButton = document.querySelector(".cta-form__button");
+const emailInput = document.getElementById("email");
+const errorSpan = document.querySelector(".cta-form__error");
+// FUNCTIONS & METHODS
+function validateEmail() {
+    if (!emailInput.value.includes("@")) {
+        errorSpan.style.display = "block";
+        errorSpan.innerText = "Please enter a valid email address";
+    }
+}
+// INITIALIZE & EVENT LISTENERS
+submitButton.addEventListener("click", validateEmail);
+
+//# sourceMappingURL=index.44983732.js.map
